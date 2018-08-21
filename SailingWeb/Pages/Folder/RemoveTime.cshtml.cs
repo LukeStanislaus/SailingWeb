7a122770e3ac54298125fbe6abc34eb641646bd1
Luke Stanislaus <luke@stanislaus.co.uk>
2018-08-21 10:12:51 +0100

    update times

---
+++ SailingWeb/Pages/Folder/RemoveTime.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using SailingWeb.Data;
+
+namespace SailingWeb.Pages.Folder
+{
+    public class RemoveTimeModel : PageModel
+    {
+        public void OnGet(string name, int lapNumber)
+        {
+            var boat1 = JsonConvert.DeserializeObject<BoatsTidy>(name.ToString());
+            foreach (KeyValuePair<BoatsTidy, List<BoatLap>> x in ManageRaceModel.Race.Item2)
+                if (x.Key.Boat.Equals(boat1.Boat) && x.Key.BoatNumber.Equals(boat1.BoatNumber) && x.Key.Crew.Equals(boat1.Crew) &&
+                    x.Key.Name.Equals(boat1.Name) && x.Key.Notes.Equals(boat1.Notes) &&
+                    x.Key.Py.Equals(boat1.Py))
+                {
+                    x.Value.OrderBy(z => z.LapNumber);
+                    for (int i = (lapNumber); i < (x.Value.Count +1); i++)
+                    {
+                        try
+                        {
+                            if (x.Value[i] != null)
+                            {
+                                x.Value[i].LapNumber = x.Value[i].LapNumber - 1;
+                                x.Value[i - 1] = x.Value[i];
+
+                            }
+                        }
+                        catch
+                        {
+                            x.Value.Remove(x.Value[i - 1]);
+                        }
+                    }
+                }
+
+                    
+        }
+    }
+}
