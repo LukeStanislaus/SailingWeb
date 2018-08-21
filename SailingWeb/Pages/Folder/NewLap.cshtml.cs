8a5f28c2a89f90f6691e85cef00f51b82a367f91
Luke Stanislaus <luke@stanislaus.co.uk>
2018-08-21 16:22:05 +0100

    changes!!!!!!!!!

--- SailingWeb/Pages/Folder/NewLap.cshtml.cs
+++ SailingWeb/Pages/Folder/NewLap.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,22 +12,35 @@ namespace SailingWeb.Pages.Folder
 {
     public class NewLapModel : PageModel
     {
+
+        public static int x = 0;
         public void OnGet(string boat, DateTime lapTime, int lapNumber)
         {
-            var boat1 = JsonConvert.DeserializeObject<BoatsTidy>(boat.ToString());
+            if(x==0)
+            {
+                bool z = true;
+                x++;
+                var boat1 = JsonConvert.DeserializeObject<BoatsTidy>(boat.ToString());
 
-            var lapTime2 = lapTime.Subtract(ManageRaceModel.StartTime);
+                var lapTime2 = lapTime.Subtract(ManageRaceModel.StartTime);
 
-            BoatLap lap = new BoatLap(lapNumber, lapTime2);
-            foreach (KeyValuePair<BoatsTidy, List<BoatLap>> x in ManageRaceModel.Race.Item2)
-                if (x.Key.Boat.Equals(boat1.Boat) && x.Key.BoatNumber.Equals(boat1.BoatNumber) && x.Key.Crew.Equals(boat1.Crew) &&
-                    x.Key.Name.Equals(boat1.Name) && x.Key.Notes.Equals(boat1.Notes) &&
-                    x.Key.Py.Equals(boat1.Py))
-                {
-                    ManageRaceModel.Race.Item2[x.Key].Add(lap);
-                }
-
-
+                BoatLap lap = new BoatLap(lapNumber, lapTime2);
+                foreach (KeyValuePair<BoatsTidy, List<BoatLap>> x in ManageRaceModel.Race.Item2)
+                    if (x.Key.Boat.Equals(boat1.Boat) && x.Key.BoatNumber.Equals(boat1.BoatNumber) && x.Key.Crew.Equals(boat1.Crew) &&
+                        x.Key.Name.Equals(boat1.Name) && x.Key.Notes.Equals(boat1.Notes) &&
+                        x.Key.Py.Equals(boat1.Py))
+                    {
+                        foreach (var y in x.Value)
+                        {
+                            if (y.LapNumber == lap.LapNumber)
+                                z = false; break;
+                            
+                        }
+                        if(z)
+                        ManageRaceModel.Race.Item2[x.Key].Add(lap);
+                    }
+                x--;
+            }
             
         }
     }
